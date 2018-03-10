83da891f5e98eb0ac454538e39e88540a7a3f89f
Josh Mcguigan <joshmcg88@gmail.com>
2018-03-10 07:28:06 -0700

    Creacted url builders object

--- src/actions.js
+++ src/actions.js
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
-const urlBuilderZipper = (url, args) => {
-    // url = ['https://hacker-news.firebaseio.com/v0/item/', '.json']
-    // args = ['1234']
-    let fullURL = url[0];
-    args.forEach((arg, i)=>{
-        fullURL += arg + url[i+1];
-    });
-    // fullURL = 'https://hacker-news.firebaseio.com/v0/item/1234.json'
-    return fullURL;
+const urlBuilders = {
+    zipper: (url, args) => {
+        // url = ['https://hacker-news.firebaseio.com/v0/item/', '.json']
+        // args = ['1234']
+        let fullURL = url[0];
+        args.forEach((arg, i)=>{
+            fullURL += arg + url[i+1];
+        });
+        // fullURL = 'https://hacker-news.firebaseio.com/v0/item/1234.json'
+        return fullURL;
+    }
 };
 
 const apiCall = (url, urlBuilder) => {
@@ -37,4 +39,4 @@ const apiCall = (url, urlBuilder) => {
 };
 
 export const getTopStories = apiCall('https://hacker-news.firebaseio.com/v0/topstories.json');
-export const loadStoryDetails = apiCall(['https://hacker-news.firebaseio.com/v0/item/', '.json'], urlBuilderZipper);
+export const loadStoryDetails = apiCall(['https://hacker-news.firebaseio.com/v0/item/', '.json'], urlBuilders.zipper);
